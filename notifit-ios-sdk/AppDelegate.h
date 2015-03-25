63c9bc67029e066423f0ca3a7526733eb01e7dc6
Lukáš Hromadník <lukas.hromadnik@gmail.com>
2015-03-25 12:59:50 +0100

    Project creation

---
+++ notifit-ios-sdk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  notifit-ios-sdk
+//
+//  Created by Lukáš Hromadník on 25.03.15.
+//  Copyright (c) 2015 Lukáš Hromadník. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ notifit-ios-sdkTests/notifit_ios_sdkTests.m
@@ -0,0 +1,40 @@
+//
+//  notifit_ios_sdkTests.m
+//  notifit-ios-sdkTests
+//
+//  Created by Lukáš Hromadník on 25.03.15.
+//  Copyright (c) 2015 Lukáš Hromadník. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface notifit_ios_sdkTests : XCTestCase
+
+@end
+
+@implementation notifit_ios_sdkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
